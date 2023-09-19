3fe23d11b071e35b01d2890b15cb49317945422f
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2023-09-19 13:18:09 +0300

    Rest of the flaws added

--- server/pages/urls.py
+++ server/pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import homePageView, transferView
+from .views import homePageView, transferView, checkView, transactionsView
 
 urlpatterns = [
     path('', homePageView, name='home'),
-    path('transfer/', transferView, name='transfer')
+    path('transfer/', transferView, name='transfer'),
+    path('check/', checkView, name='check'),
+    path('transactions/<str:name>', transactionsView, name='transactionsView')
 ]
